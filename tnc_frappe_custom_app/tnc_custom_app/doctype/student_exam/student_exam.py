d16aacb13dd3b24dd37af2a4d2cdd0182a5f6b12
Srikanth <srikanthp@360ithub.com>
2024-09-10 11:04:18 +0530

    sep10-11:05AM

--- tnc_frappe_custom_app/tnc_custom_app/doctype/student_exam/student_exam.py
+++ tnc_frappe_custom_app/tnc_custom_app/doctype/student_exam/student_exam.py
@@ -7,15 +7,6 @@ from frappe.model.document import Document
 
 class StudentExam(Document):
     pass
-<<<<<<< HEAD
-
-
-####################################Vatsal's Working Modified code for conditional live and enqueing bulk operations start ######
- 
- 
-####################################Vatsal's Working Modified code for enqueing bulk operations start #########################
- 
-=======
     # def on_update(self):
     #     # Ensure that exam_date is not empty before proceeding
     #     if self.exam_date:
@@ -41,7 +32,6 @@ class StudentExam(Document):
 
 ####################################Vatsal's Working Modified code for enqueing bulk operations start #########################
 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
 def student_and_result_validation_and_creation(student_data):
     try:
         existing_student = frappe.db.exists('Student', {'mobile': student_data['mobile']})
@@ -59,11 +49,7 @@ def student_and_result_validation_and_creation(student_data):
             student_id = new_student.name
         else:
             student_id = frappe.get_value('Student', {'mobile': student_data['mobile']}, 'name')
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
         new_test_series_result = frappe.get_doc({
             'doctype': 'Student Results',
             'student_id': student_id,
@@ -81,28 +67,6 @@ def student_and_result_validation_and_creation(student_data):
             'system_imported': 1
         })
         new_test_series_result.insert()
-<<<<<<< HEAD
- 
-        frappe.db.set_value('Students Master Data', student_data['name'], 'imported', 1)
-        
-        # print(completed_records)
- 
-    except Exception as e:
-        # print(e)
-        frappe.log_error(frappe.get_traceback(), f"Error syncing student master data {student_data['name']} in Student Exam {student_data['student_name']}")
- 
- 
-@frappe.whitelist()
-def student_process_data(name, limit=1500):
-    try:
-        total_records = frappe.db.count('Students Master Data', filters={'imported': 0, 'imported_batch_id': name})
-        if total_records > limit:
-            # Enqueue the background job
-            frappe.enqueue('tnc_frappe_custom_app.tnc_custom_app.doctype.student_exam.student_exam.process_students_in_background',
-                        queue='long',
-                        timeout=6000,
-                        name=name,
-=======
 
         frappe.db.set_value('Students Master Data', student_data['name'], 'imported', 1)
         
@@ -132,22 +96,12 @@ def student_process_data(name, limit=1500):
                         queue='long', 
                         timeout=6000, 
                         name=name, 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
                         # limit=limit
                         )
             return {"status":True,"enqueued":True,"msg":"Data syncing is Enqueued Successfully!"}
         else:
             response_realtime_data_processing=process_data_realtime(name)
             return response_realtime_data_processing
-<<<<<<< HEAD
- 
-    except Exception as e:
-        return {"status": False, "msg": str(e)}
-    # process_students_in_background(name, start, limit)
- 
- 
- 
-=======
 
     except Exception as e:
         return {"status": False, "msg": str(e)}
@@ -155,20 +109,14 @@ def student_process_data(name, limit=1500):
 
 
 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
 def process_students_in_background(name):
     # print("Function Calleddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
     try:
         students_exam_doc = frappe.get_doc('Student Exam', name)
         students_master_data = frappe.get_all('Students Master Data', filters={
             'imported': 0, 'imported_batch_id': name
-<<<<<<< HEAD
-        }, fields=['name', 'student_name', 'mobile', 'state', 'rank', 'total_marks', 'district',
-                   'total_right', 'total_wrong', 'total_skip', 'percentage', 'imported_batch_id'],
-=======
         }, fields=['name', 'student_name', 'mobile', 'state', 'rank', 'total_marks', 'district', 
                    'total_right', 'total_wrong', 'total_skip', 'percentage', 'imported_batch_id'], 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
             # start=start,
             # limit=limit,
                      )
@@ -236,25 +184,6 @@ def process_students_in_background(name):
     
         return {"status": True, "msg": f"Data Processed Successfully"}
     except Exception as e:
-<<<<<<< HEAD
-        frappe.log_error(frappe.get_traceback(), "Error in background sync")
-        return {"status": False, "msg": f"Error in Background Processing Data: {str(e)}"}
- 
-####################################Vatsal's Working Modified code for enqueing bulk operations end #########################
- 
- 
-#################################Vatsal's Working Modified code for % based progress publish and exception handling start #######
- 
-@frappe.whitelist()
-def process_data_realtime(name):
-    try:
-        students_exam_doc = frappe.get_doc('Student Exam', name)
-        # Fetch all records from Students Master Data where imported is not checked
-        students_master_data = frappe.get_all('Students Master Data', filters={
-            'imported': 0, 'imported_batch_id': name
-        }, fields=['name', 'student_name', 'mobile', 'state', 'rank', 'total_marks', 'district', 'total_right', 'total_wrong', 'total_skip', 'percentage', 'imported_batch_id'],
-        # start=start,
-=======
         # students_exam_doc = frappe.get_doc('Student Exam', name)
         students_exam_doc.status="Failed In Queue"
         students_exam_doc.save()
@@ -277,7 +206,6 @@ def process_data_realtime(name):
             'imported': 0, 'imported_batch_id': name
         }, fields=['name', 'student_name', 'mobile', 'state', 'rank', 'total_marks', 'district', 'total_right', 'total_wrong', 'total_skip', 'percentage', 'imported_batch_id'], 
         # start=start, 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
         # limit=limit
         )
     
@@ -342,11 +270,7 @@ def process_data_realtime(name):
             #     continue  # Continue to the next student even if an error occurs
             completed_records += 1
             progress = int((completed_records / total_records) * 100)
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
             if progress > publish_iteration:
                 frappe.publish_realtime("sync_progress", {
                     "completed_records": completed_records,
@@ -364,12 +288,6 @@ def process_data_realtime(name):
         return {"status": True, "msg": f"Data Processed Successfully"}
     except Exception as e:
         print(e)
-<<<<<<< HEAD
-        return {"status": False, "msg": f"Error in Processing Data: {str(e)}"}
- 
-###########################################Vatsal's Modified code for % based progress publish and exception handling end#######
-####################################Vatsal's Working Modified code for conditional live and enqueing bulk operations end ######
-=======
         students_exam_doc.status="Failed In Queue"
         students_exam_doc.save()
         return {"status": False, "msg": f"Error in Processing Data: {str(e)}"}
@@ -471,73 +389,12 @@ def process_data_realtime(name):
 #     frappe.db.commit()
     
 #     return "success"
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
 
 
-# @frappe.whitelist()
-# def student_process_data(name, start=0, limit=10000):
-#     try:
-#         students_exam_doc = frappe.get_doc('Student Exam', name)
-        
-#         # Fetch records from Students Master Data where imported is not checked
-#         students_master_data = frappe.get_all('Students Master Data', filters={
-#             'imported': 0, 'imported_batch_id': name
-#         }, fields=['name', 'student_name', 'mobile', 'state', 'rank', 'total_marks', 'district', 'total_right', 'total_wrong', 'total_skip', 'percentage', 'imported_batch_id'], start=start, limit=limit)
-        
-#         total_records = frappe.db.count('Students Master Data', filters={'imported': 0, 'imported_batch_id': name})
 
-#         # If no records are found, return a message indicating no data to sync
-#         if not students_master_data:
-#             return {"status": "no_data", "progress": 100}
-        
-#         completed_records = start
-#         publish_iteration = 0
 
-#         # Loop through the fetched data and sync it
-#         for student_data in students_master_data:
-#             try:
-#                 # Check if the student already exists in the 'Student' doctype by mobile number
-#                 existing_student = frappe.db.exists('Student', {'mobile': student_data['mobile']})
-#                 if not existing_student:
-#                     # Create a new student record in the 'Student' doctype
-#                     new_student = frappe.get_doc({
-#                         'doctype': 'Student',
-#                         'student_name': student_data['student_name'],
-#                         'mobile': student_data['mobile'],
-#                         'state': student_data['state'],
-#                         'district': student_data['district'],
-#                         'system_imported': 1,
-#                         'student_batch_id': student_data['imported_batch_id'],
-#                     })
-#                     new_student.insert()
-#                     student_id = new_student.name
-#                 else:
-#                     # Fetch the existing student's ID
-#                     student_id = frappe.get_value('Student', {'mobile': student_data['mobile']}, 'name')
 
-#                 # Create a new 'Student Results' record
-#                 new_test_series_result = frappe.get_doc({
-#                     'doctype': 'Student Results',
-#                     'student_id': student_id,
-#                     'student_name': student_data['student_name'],
-#                     'student_mobile': student_data['mobile'],
-#                     'exam_date': student_data.get('date'),
-#                     'exam_code': student_data.get('exam_code'),
-#                     'rank': student_data.get('rank'),
-#                     'total_marks': student_data.get('total_marks'),
-#                     'total_right': student_data.get('total_right'),
-#                     'total_wrong': student_data.get('total_wrong'),
-#                     'total_skip': student_data.get('total_skip'),
-#                     'percentage': student_data.get('percentage'),
-#                     'batch_id': student_data.get('imported_batch_id'),
-#                     'system_imported': 1
-#                 })
-#                 new_test_series_result.insert()
 
-<<<<<<< HEAD
-#                 # Mark the student record in 'Students Master Data' as imported
-#                 frappe.db.set_value('Students Master Data', student_data['name'], 'imported', 1)
-=======
 ############## Adding the Validations to the color Generation table ###############
 
 
@@ -596,36 +453,9 @@ def process_data_realtime(name):
 #             frappe.throw(_("Please ensure that 'Green' rank range is defined."))
 #         if not yellow_end_to:
 #             frappe.throw(_("Please ensure that 'Yellow' rank range is defined."))
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
 
-#                 completed_records += 1
-#                 progress = int((completed_records / total_records) * 100)
 
-#                 if progress > publish_iteration:
-#                     frappe.publish_realtime("sync_progress", {
-#                         "completed_records": completed_records,
-#                         "total_records": total_records,
-#                         "progress": progress,
-#                     })
-#                     publish_iteration += 1
 
-<<<<<<< HEAD
-#             except Exception as e:
-#                 # Log the error but allow the process to continue for other records
-#                 frappe.log_error(frappe.get_traceback(), f"Error syncing student {student_data['name']} in Student Exam {name}")
-#                 continue
-
-#         # Update the status of the 'Student Exam' document after syncing is complete
-#         students_exam_doc.status = 'Data Synced'
-#         students_exam_doc.save()
-
-#         return {"status": "success", "progress": 100}
-
-#     except Exception as e:
-#         # Log the error if the entire function fails
-#         frappe.log_error(frappe.get_traceback(), f"Error processing student data for exam {name}")
-#         return {"status": "error", "message": str(e), "progress": 0}
-=======
 ################################Delete the specific exam ID in the Student Results doctype #################################
 
 import frappe
@@ -644,4 +474,3 @@ def delete_student_results(exam_id):
     else:
         pass
         # return f"No student results found for Exam {exam_id}"
->>>>>>> b7246a6c02dd805979784c9384208f2d08e619bd
