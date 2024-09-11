5947f0b73b33ac9f15f2259b604b3d435f2bcac0
Srikanth <srikanthp@360ithub.com>
2024-09-11 19:03:10 +0530

    sep11-WA

--- tnc_frappe_custom_app/custom_whatsapp.py
+++ tnc_frappe_custom_app/custom_whatsapp.py
@@ -1,77 +1,84 @@
 ############################### Below code is sending the whatsapp message with particular subject only ###################################
-# import frappe
-# import requests
-# from frappe.utils import now
+import frappe
+import requests
+from frappe.utils import now
 
-# @frappe.whitelist()
-# def send_whatsapp_pdf_message(name, mobile_number, student_name, message):
-#     # Fetch the instance_id from the "Admin Settings" doctype
-#     base_url = frappe.utils.get_url()
-#     admin_settings = frappe.get_doc('Admin Settings')
-#     instance_id = admin_settings.instance_id
-    
-#     # Prepend +91 to the mobile number if not already present
-#     if not mobile_number.startswith("91"):
-#         mobile_number = "91" + mobile_number
+@frappe.whitelist()
+def send_whatsapp_pdf_message(name, mobile_number, student_name, message):
+    # Fetch the instance_id from the "Admin Settings" doctype
+    base_url = frappe.utils.get_url()
+    admin_settings = frappe.get_doc('Admin Settings')
+    instance_id = admin_settings.instance_id
 
-#     # Generate the dynamic file URL
-#     # file_url = "https://tourism.gov.in/sites/default/files/2019-04/dummy-pdf_2.pdf"
-#     file_url = f"{base_url}/api/method/frappe.utils.print_format.download_pdf?doctype=Student&name={name}&format=Student%20Results%20PF&no_letterhead=0&letterhead=TNC%20Logo&settings=%7B%7D&_lang=en/{student_name}.pdf"
-#     text_message = f"Dear {student_name}, Check your results."
+    # Construct the API URL
+    try:
+        # Make the API request
+        base_url = frappe.utils.get_url()
+        # response = requests.get(api_url)
+        # link = f"{base_url}/api/method/frappe.utils.print_format.download_pdf?doctype=Student&name={name}&format=Student%20Results%20PF&no_letterhead=0&letterhead=TNC%20Logo&settings=%7B%7D&_lang=en/{student_name}pdf"
+        link = f"{base_url}method/frappe.utils.print_format.download_pdf?doctype=Student&name={name}&format=Student%20Results%20PF&no_letterhead=0&letterhead=TNC%20Logo&settings=%7B%7D&_lang=en/TNC REPORT CARD.pdf"
+        # link = "https://online.lsaoffice.com/api/method/frappe.utils.print_format.download_pdf?doctype=Sales%20Order&name=SAL-ORD-2023-00262&format=Sales%20Order%20Format&no_letterhead=0&letterhead=LSA&settings=%7B%7D&_lang=en/Invoice.pdf"
 
-#     # Construct the API URL
-#     api_url = f"https://wts.vision360solutions.co.in/api/sendFileWithCaption?token={instance_id}&phone={mobile_number}&link={file_url}&message={text_message}"    
-    
-#     try:
-#         # Make the API request
-#         response = requests.get(api_url)
-#         response_data = response.json()  # Parse the JSON response
-#         print(response_data)
+        url = "https://wts.vision360solutions.co.in/api/sendFileWithCaption"
+        params_1 = {
+            "token": instance_id,
+            "phone": f"91{mobile_number}",
+            "message": message,
+            "link": link
+        } 
+        response = requests.post(url, params=params_1)
+        response_data = response.json()  # Parse the JSON response
+        # print(response_data)
 
-#         # Check for success based on the response data
-#         if response_data.get('status') == 'success':
-#             message_status = "success"
-#             message_ids = response_data.get('data', {}).get('messageIDs', [])
-#             frappe.logger().info(f"WhatsApp message sent successfully to {mobile_number}")
-#         else:
-#             message_status = "Failed"
-#             message_ids = []
-#             frappe.logger().error(f"Failed to send WhatsApp message to {mobile_number}. Response: {response_data}")
-#     except Exception as e:
-#         frappe.logger().error(f"Error sending WhatsApp message to {mobile_number}: {str(e)}")
-#         frappe.log_error(frappe.get_traceback(), "WhatsApp Message Sending Failed")
-#         message_status = "Failed"
-#         message_ids = []
-
-#     # Log the message in WhatsApp Message Log doctype
-#     try:
-#         frappe.get_doc({
-#             'doctype': 'WhatsApp Message Log',
-#             'sender': frappe.session.user,   # Sender is the logged-in user
-#             'send_date': now(),              # Send date is the current time
-#             'message_type': 'Custom',        # Set custom message type
-#             'message': message,              # Message from the client script
-#             'student_id': name,              # Student ID passed from client
-#             'status': message_status,        # Log the status (Success/Failed)
-#             'message_id': ', '.join(message_ids)  # Join message IDs if any
-#         }).insert(ignore_permissions=True)
+        # Check for success based on the response data
+        if response_data.get('status') == 'success':
+            message_status = "Success"
+            message_ids = response_data.get('data', {}).get('messageIDs', [])
+            frappe.logger().info(f"WhatsApp message sent successfully to {mobile_number}")
+        else:
+            message_status = "Failed"
+            message_ids = []
+            frappe.logger().error(f"Failed to send WhatsApp message to {mobile_number}. Response: {response_data}")
+    except Exception as e:
+        frappe.logger().error(f"Error sending WhatsApp message to {mobile_number}: {str(e)}")
+        frappe.log_error(frappe.get_traceback(), "WhatsApp Message Sending Failed")
+        message_status = "Failed"
+        message_ids = []
+    # Log the message in WhatsApp Message Log doctype
+    try:
+        frappe.get_doc({
+            'doctype': 'WhatsApp Message Log',
+            'sender': frappe.session.user,   # Sender is the logged-in user
+            'send_date': now(),              # Send date is the current time
+            'message_type': 'Custom',        # Set custom message type
+            'message': message,              # Message from the client script
+            'student_id': name,              # Student ID passed from client
+            'status': message_status,        # Log the status (Success/Failed)
+            'message_id': ', '.join(message_ids)  # Join message IDs if any
+        }).insert(ignore_permissions=True)
         
-#         frappe.logger().info(f"WhatsApp message log created for student {name} with status {message_status}")
-#     except Exception as log_error:
-#         frappe.log_error(frappe.get_traceback(), "Failed to create WhatsApp Message Log")
+        frappe.logger().info(f"WhatsApp message log created for student {name} with status {message_status}")
+    except Exception as log_error:
+        frappe.log_error(frappe.get_traceback(), "Failed to create WhatsApp Message Log")
 
-#     return {'status': message_status}
-
-
-
+    # return {'status': message_status}
+    return {"status": 'success', "msg": "WhatsApp message sent successfully!"}
 
 
-
-
-
-
-
-
+@frappe.whitelist()
+def valiadting_user_for_bulk_wa_msg():
+    user = frappe.session.user
+    if user=="Administrator":
+        return {"status": True, "msg": "User has the required role."}
+    
+    admin_setting_doc = frappe.get_doc("Admin Settings")
+    auhenticated_users=[]
+    for i in admin_setting_doc.whatsapp_access:
+        auhenticated_users.append(i.user)
+    
+    if user not in auhenticated_users :
+        return {"status": False, "msg": "User does not have the required role to send Bulk WhatsApp messages."}
+    return {"status": True, "msg": "User has the required role."}
 
 
 
--- tnc_frappe_custom_app/tnc_custom_app/doctype/student/student_list.js
+++ tnc_frappe_custom_app/tnc_custom_app/doctype/student/student_list.js
@@ -1,34 +1,34 @@
 
-// frappe.listview_settings['Student'] = {
-//     onload: function(listview) {
-//         listview.page.add_inner_button(__('Delete Data'), function() {
-//             // Show a confirmation dialog
-//             frappe.confirm(
-//                 'Are you sure you want to delete all records?',
-//                 function() {
-//                     // If confirmed, make a Frappe call to the server
-//                     frappe.call({
-//                         method: 'tnc_frappe_custom_app.delete_import_data_master_students.delete_all_records_in_student',
-//                         callback: function(response) {
-//                             if (response.message === 'success') {
-//                                 frappe.show_alert({
-//                                     message: __('All records have been deleted successfully.'),
-//                                     indicator: 'green'
-//                                 });
-//                                 listview.refresh(); // Refresh the list view to reflect the changes
-//                             } else {
-//                                 frappe.show_alert({
-//                                     message: __('There was an issue deleting the records.'),
-//                                     indicator: 'red'
-//                                 });
-//                             }
-//                         }
-//                     });
-//                 }
-//             );
-//         });
-//     }
-// };
+frappe.listview_settings['Student'] = {
+    onload: function(listview) {
+        listview.page.add_inner_button(__('Delete Data'), function() {
+            // Show a confirmation dialog
+            frappe.confirm(
+                'Are you sure you want to delete all records?',
+                function() {
+                    // If confirmed, make a Frappe call to the server
+                    frappe.call({
+                        method: 'tnc_frappe_custom_app.delete_import_data_master_students.delete_all_records_in_student',
+                        callback: function(response) {
+                            if (response.message === 'success') {
+                                frappe.show_alert({
+                                    message: __('All records have been deleted successfully.'),
+                                    indicator: 'green'
+                                });
+                                listview.refresh(); // Refresh the list view to reflect the changes
+                            } else {
+                                frappe.show_alert({
+                                    message: __('There was an issue deleting the records.'),
+                                    indicator: 'red'
+                                });
+                            }
+                        }
+                    });
+                }
+            );
+        });
+    }
+};
 
 ///////////////////Below is the BUlk message button whatsapp ///////////////////////////////////////////////
 
