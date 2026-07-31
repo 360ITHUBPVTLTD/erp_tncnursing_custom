fe1f4a94fce8e5e60c2f6def76b75b7e8f401a53
Anurag <anurag.d@360ithub.com>
2026-07-31 18:54:55 +0530

    patch commented

--- tnc_frappe_custom_app/patches/v1_0/backfill_task_reporter.py
+++ tnc_frappe_custom_app/patches/v1_0/backfill_task_reporter.py
@@ -1,12 +1,12 @@
-import frappe
+# import frappe
 
 
-def execute():
-	frappe.reload_doctype("Task")
+# def execute():
+# 	frappe.reload_doctype("Task")
 
-	frappe.db.sql("""
-		UPDATE `tabTask` t
-		JOIN `tabUser` u ON u.name = t.owner
-		SET t.task_reporter = t.owner, t.task_reporter_name = u.full_name
-		WHERE t.task_reporter IS NULL OR t.task_reporter = ''
-	""")
+# 	frappe.db.sql("""
+# 		UPDATE `tabTask` t
+# 		JOIN `tabUser` u ON u.name = t.owner
+# 		SET t.task_reporter = t.owner, t.task_reporter_name = u.full_name
+# 		WHERE t.task_reporter IS NULL OR t.task_reporter = ''
+# 	""")
